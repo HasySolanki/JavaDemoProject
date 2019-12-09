05e7123fa0f0d50edfcdb7174f2df7ed9a8aaf8a
Administrator <Administrator@dm>
2019-12-09 15:01:21 +0530

    Third commit

---
+++ JavaDemoTesting/src/DemoPck/javaTest.java
@@ -0,0 +1,11 @@
+package DemoPck;
+
+public class javaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a github commit and push  by java class");
+
+	}
+
+}
