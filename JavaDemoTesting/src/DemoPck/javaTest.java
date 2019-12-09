e5b7cea5e1d20ce5abaedd8cca2b1063e090a2e6
HasySolanki <designmate.hasmukh@gmail.com>
2019-12-09 15:03:34 +0530

    Update javaTest.java

--- JavaDemoTesting/src/DemoPck/javaTest.java
+++ JavaDemoTesting/src/DemoPck/javaTest.java
@@ -5,6 +5,12 @@ public class javaTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a github commit and push  by java class");
+		
+		for(int i=0;i<=10;i++){
+		
+			System.out.println("Values for i variable:-"+i);
+		
+		}
 
 	}
 
